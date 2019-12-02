5760bc56904153a7385cc151205a47d348088e6f
waltyou <waltyou@163.com>
2019-12-02 10:58:15 +0800

    add readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 1. [x] syntax highlighting
 2. [x] auto complete
-3. [x] show syntax error
+3. [] show syntax error
 
 ## tech stack
 
@@ -14,7 +14,7 @@
 2. vue
 3. js
 
-### 2. backend
+### 2. backend //TODO
 
 1. spark-catalyst
 2. scalatra
---
+++ editor-fronted/src/components/SparksqlEditor.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="md-subhead">
+    <p>Spark Sql Online Editor</p>
+    <div>
+      <!-- codemirror -->
+      <codemirror v-model="code" :options="cmOption">
+      </codemirror>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// cm common
+import 'codemirror/theme/cobalt.css'
+import 'codemirror/addon/hint/show-hint.css'
+import 'codemirror/addon/hint/show-hint.js'
+import 'codemirror/addon/lint/lint.css'
+import 'codemirror/addon/lint/lint.js'
+
+import 'codemirror/addon/hint/sql-hint.js'
+import 'codemirror/mode/sql/sql.js'
+
+export default {
+  name: 'SparksqlEditor',
+  data () {
+    const code =
+`
+select a from b;
+`
+    return {
+      code,
+      cmOption: {
+        tabSize: 4,
+        styleActiveLine: true,
+        lineNumbers: true,
+        extraKeys: {'Ctrl': 'autocomplete'},
+        line: true,
+        mode: 'text/x-sparksql',
+        theme: 'cobalt',
+        viewportMargin: Infinity,
+        gutters: ['CodeMirror-lint-markers'],
+        lint: true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.CodeMirror {
+  height: 1100px;
+  font-size: 16px;
+}
+</style>
